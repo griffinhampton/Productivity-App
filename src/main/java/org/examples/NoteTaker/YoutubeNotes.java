107944e185e5f0c4c2d7dc4ba998bf1fd89fb70f
Griffin Hampton <gmhamp01@louisville.edu>
2025-02-12 16:07:31 -0500

    second update

--- src/main/java/org/examples/NoteTaker/YoutubeNotes.java
+++ src/main/java/org/examples/NoteTaker/YoutubeNotes.java
@@ -1,4 +1,21 @@
 package org.examples.NoteTaker;
 
+
+import java.net.URL;
+import org.examples.NoteTaker.enums.users.userClasses.IuserClasses;
+
 public class YoutubeNotes {
+    IuserClasses userClasses;
+    URL url;
+    public YoutubeNotes(URL url) {
+        this.url = url;
+    }
+    public URL getUrl() {
+        return url;
+    }
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+
 }
